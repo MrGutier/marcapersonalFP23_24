707cf65eb07e325b67150091b1e2ffcb5de8ee0d
Sergio Gutiérrez Valverde <1833914@alu.murciaeduca.es>
2023-11-30 08:23:28 +0100

    añadidas migraciones

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
+        $estudiante = new \App\Models\Estudiante;
+        $estudiante->nombre = 'Juan';
+        $estudiante->apellidos = 'Martínez';
+        $estudiante->direccion = 'Dirección de Juan';
+        $estudiante->votos = 130;
+        $estudiante->confirmado = true;
+        $estudiante->ciclo = 'DAW';
+        $estudiante->save();
     }
 }
